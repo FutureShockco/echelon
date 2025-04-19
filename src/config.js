2b55ae312856a5f1225536ffeae536a6bef4e611
Burak Akdogan <66314810+burak-akdogan@users.noreply.github.com>
2025-04-19 15:55:04 -0700

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ let config = {
     history: {
         0: {
             // Steem block to start streaming from
-            steemStartBlock: 94852377,
+            steemStartBlock: 94854484,
             steemBlockDelay: 1,
             // this is the block 0 configuration for mainnet
             accountPriceBase: 20000,
